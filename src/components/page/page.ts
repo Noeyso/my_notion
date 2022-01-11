8a9f3225ba71e533fb01c83989c81a6aa77527c5
noeyso <soyeon9819@gmail.com>
2022-01-11 19:51:49 +0900

    DI(Dependency Injection) 리팩토링

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,9 +3,18 @@ export interface Composable {
   addChild(child: Component): void;
 }
 type OnCloseListener = () => void;
-class PageItemComponent
+
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+//생성자를 정의하는 타입
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable
+  implements SectionContainer
 {
   private closeListener?: OnCloseListener;
   constructor() {
@@ -35,11 +44,11 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super(`<ul class="page"></ul>`);
   }
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
     item.setOnCloseListener(() => {
